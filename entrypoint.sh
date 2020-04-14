d646bf6074e8c7b596af8fb37735d1260f30aa1f
Giovanny Andres Gongora Granada (Gioyik) <gioyik@gmail.com>
2020-04-14 15:28:43 -0500

    1.1.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 
 if [[ "$2" = "yes" ]]
 then
-  NCM_TOKEN=$1 ncm report --long
+  IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report --long
 elif [[ "$3" = "yes" ]]
 then
-  NCM_TOKEN=$1 ncm report --compliance
+  IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report --compliance
 elif [[ "$4" = "yes" ]]
 then
-  NCM_TOKEN=$1 ncm report --security
+  IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report --security
 else
-  NCM_TOKEN=$1 ncm report
+  FORCE_COLOR=2 IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report
 fi
