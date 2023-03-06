44212c4a9a3644cd71db9797ba34d8bb126ba2a4
MinwooJung-ns <babymealapp@gmail.com>
2023-03-06 14:56:41 +0900

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,4 @@
 
 npm i
 
-if [[ "$2" = "yes" ]]
-then
-  FORCE_COLOR=1 IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report --json --long --gate > report.json
-elif [[ "$3" = "yes" ]]
-then
-  FORCE_COLOR=1 IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report --json --compliance --gate > report.json
-elif [[ "$4" = "yes" ]]
-then
-  FORCE_COLOR=1 IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report --json --security > report.json
-else
-  FORCE_COLOR=1 IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report --json > report.json
-fi
+FORCE_COLOR=1 IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report --gate > report.json
