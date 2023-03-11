7aeece4a5960dca67085ffdc8be1a5b4982a0e76
MinwooJung-ns <babymealapp@gmail.com>
2023-03-11 12:00:32 +0900

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ npm i
 FORCE_COLOR=1 IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report --gate > report.json
 cat report.json
 output="$(cat report.json)"
-echo 'echo "::error file=app.js,line=1::Missing semicolon"  > ncm.sh
+echo 'echo "::error file=app.js,line=1::Missing semicolon"'  > ncm.sh
 chmod +x ncm.sh
