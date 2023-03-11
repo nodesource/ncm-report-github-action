1a5b23616fd1df4d45c35505a0ddf6d543e58a97
MinwooJung-ns <babymealapp@gmail.com>
2023-03-11 10:21:09 +0900

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ npm i
 
 FORCE_COLOR=1 IS_GITHUB_ACTION=true NCM_TOKEN=$1 ncm report --gate > report.json
 cat report.json
-cat report.json > "$GITHUB_OUTPUT"
+echo cat report.json > "$GITHUB_OUTPUT"
+output="$(cat report.json)"
+echo "::set-output name=ncm::$output" 
